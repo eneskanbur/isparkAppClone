49fec2726b445e8dd8bd40dedad4d3f3473e1959
Enes Kanbur <eneskanbur02@hotmail.com>
2025-07-09 16:08:22 +0300

    Firebase Auth is added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
     kotlin("plugin.serialization") version "2.0.21"
 }
 
@@ -59,6 +60,10 @@ dependencies {
     implementation(libs.converter.gson)
 
     implementation(libs.glide)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+
 
 
 }
